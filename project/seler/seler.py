321763a1db402ef1a0f04640a01f9c48216c5054
Charge Goei <40865608+anlowee@users.noreply.github.com>
2020-05-30 10:43:21 +0800

    Update seler.py

--- project/seler/seler.py
+++ project/seler/seler.py
@@ -33,7 +33,7 @@ dataset = np.concatenate([set1, set5], axis=0)
 def build_model():
     model = Sequential()
     model.add(LSTM(units=4, batch_input_shape=(batch_size, look_back, features), stateful=True, return_sequences=True))
-    model.add(LSTM(units=4, batch_input_shape=(batch_size, look_back, features), stateful=True))
+    model.add(LSTM(units=4, stateful=True))
     model.add(Dense(units=features))
     model.compile(loss='mean_squared_error', optimizer='adam')
     return model
